b2a59f0bbf4e939c1da70255d214a69cce259528
Devin Chen <d3vin.chen@gmail.com>
2017-02-17 08:31:28 +0800

    samples order by first letter of sample name

--- docs/src/Samples.js
+++ docs/src/Samples.js
@@ -1,11 +1,11 @@
 /* eslint no-path-concat: 0, no-var: 0, key-spacing: 0 */
 
+// 请按照字母顺序进行排序
 export default {
-  GridBasic:                     require('fs').readFileSync(__dirname + '/../examples/GridBasic.js', 'utf8'),
-  GridOperation:                 require('fs').readFileSync(__dirname + '/../examples/GridOperation.js', 'utf8'),
-  GridPagination:                require('fs').readFileSync(__dirname + '/../examples/GridPagination.js', 'utf8'),
-  ReferBasic:                    require('fs').readFileSync(__dirname + '/../examples/ReferBasic.js', 'utf8'),
-  ReferTree:                     require('fs').readFileSync(__dirname + '/../examples/ReferTree.js', 'utf8'),
-
-  FormBasic:                     require('fs').readFileSync(__dirname + '/../examples/FormBasic.js', 'utf8')
+  FormBasic:      require('fs').readFileSync(__dirname + '/../examples/FormBasic.js', 'utf8'),
+  GridBasic:      require('fs').readFileSync(__dirname + '/../examples/GridBasic.js', 'utf8'),
+  GridOperation:  require('fs').readFileSync(__dirname + '/../examples/GridOperation.js', 'utf8'),
+  GridPagination: require('fs').readFileSync(__dirname + '/../examples/GridPagination.js', 'utf8'),
+  ReferBasic:     require('fs').readFileSync(__dirname + '/../examples/ReferBasic.js', 'utf8'),
+  ReferTree:      require('fs').readFileSync(__dirname + '/../examples/ReferTree.js', 'utf8')
 };
