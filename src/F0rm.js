14fc1b0bd6c17eaa11bd0d8595630e93feb59cc2
Devin Chen <d3vin.chen@gmail.com>
2017-02-17 15:35:56 +0800

    add checkbox

--- src/F0rm.js
+++ src/F0rm.js
@@ -47,18 +47,46 @@ export default React.createClass({
       <form className={classNames(className)}>
         {
           fieldsModel.map((fieldModel, index) => {
+            let formCtrl;
+            switch(fieldModel.type) {
+              default:
+              case 'string': // 0
+              case 'double': // 2
+              case 'ref': // 5
+              case 'enum': // 6
+                formCtrl = (
+                  <FormControl
+                    type="text"
+                    value={this.state.formData[fieldModel.id]}
+                    placeholder={fieldModel.placeholder}
+                    onChange={this.handleChange.bind(this, fieldModel.id)}
+                  />
+                );
+                break;
+              // case 'date': // 3
+              //   formCtrl = (
+              //     <DatePicker
+              //       id={id}
+              //       value={this.state.datePickerValue}
+              //       onChange={this.handleDatePickerChange.bind(this)}
+              //     />
+              //   );
+              //   break;
+              case 'boolean': // 4
+                formCtrl = (
+                  <Checkbox checked={this.state.formData[fieldModel.id]}
+                    onChange={this.handleChange.bind(this, fieldModel.id)}
+                  />
+                );
+                break;
+            }
             return (
               <FormGroup
                 key={index}
                 controlId={`formBasicText-${fieldModel.id}`}
               >
                 <ControlLabel>{fieldModel.label}</ControlLabel>
-                <FormControl
-                  type="text"
-                  value={this.state.formData[fieldModel.id]}
-                  placeholder={fieldModel.placeholder}
-                  onChange={this.handleChange.bind(this, fieldModel.id)}
-                />
+                {formCtrl}
                 <FormControl.Feedback />
               </FormGroup>
             );
