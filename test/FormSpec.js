79286307679d3945660650ad60537a5f2fd075aa
Devin Chen <d3vin.chen@gmail.com>
2017-02-18 15:14:49 +0800

    Test changing input value

--- test/FormSpec.js
+++ test/FormSpec.js
@@ -6,21 +6,29 @@ import Form from '../src/Form';
 
 function getFieldsModel() {
   return [
-    {'type': 'string', id: 'danjubianhao', 'label': '单据编号'},
-    {'type': 'string', id: 'danjuleixing', 'label': '单据类型'},
-    {'type': 'string', id: 'danjuzhuangtai', 'label': '单据状态'},
-    {'type': 'double', id: 'jine', 'label': '金额'},
-    {'type': 'string', id: 'danjuriqi', 'label': '单据日期'}
+    {type: 'string', id: 'danjubianhao', label: '单据编号'},
+    {type: 'enum', id: 'danjuleixing', label: '单据类型', placeholder: '请选择单据类型',
+      data: [
+        {key: '2631', value: '差旅费借款单'},
+        {key: '2632', value: '会议费借款单'},
+        {key: 'D3', value: '付款单'}
+      ]
+    },
+    {type: 'string', id: 'danjuzhuangtai', label: '单据状态'},
+    {type: 'double', id: 'jine', label: '金额'},
+    {type: 'date', id: 'danjuriqi', label: '单据日期'},
+    {type: 'boolean', id: 'qiyong', label: '启用'}
   ];
 }
 
 function getDefaultFormData() {
   return {
     danjubianhao: '123',
-    danjuleixing: '类型1',
+    danjuleixing: 'D3',
     danjuzhuangtai: '保存',
     jine: '12.00',
-    danjuriqi: new Date('2017-02-14').toISOString()
+    danjuriqi: new Date('2017-02-14').toISOString(),
+    qiyong: false
   };
 }
 
@@ -55,5 +63,23 @@ describe('<Form>', () => {
       className="bob"/>
     );
     assert.ok(ReactDOM.findDOMNode(instance).className.match(/\bbob\b/));
+  });
+
+  it('Should change input value', () => {
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Form
+        fieldsModel={getFieldsModel()}
+        defaultData={getDefaultFormData()}
+      />
+    );
+
+    // 修改表单中第一个文本框“单据编号”中的值
+    const node = ReactDOM.findDOMNode(instance);
+    // const firstInputNode = node.querySelector('input');
+    const firstInputNode = node.querySelectorAll('input')[0];
+    firstInputNode.value = 'test';
+    ReactTestUtils.Simulate.change(firstInputNode);
+    assert.equal(firstInputNode.value, 'test');
+    assert.equal(instance.state.formData.danjubianhao, 'test');
   });
 });
