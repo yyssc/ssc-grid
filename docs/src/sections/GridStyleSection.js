b7f74a494771efea05f1601dbecc9a642081cc5b
Devin Chen <d3vin.chen@gmail.com>
2017-03-16 09:15:06 +0800

    update docs

--- docs/src/sections/GridStyleSection.js
+++ docs/src/sections/GridStyleSection.js
@@ -10,6 +10,10 @@ export default function GridStyleSection() {
       <h3><Anchor id="grid-style">表格样式</Anchor></h3>
       <p>沿用bootstrap的样式，具体参照
       <a href="http://getbootstrap.com/css/#tables-striped">Striped rows</a>等</p>
+      <p>通过添加<code>bordered</code>为表格和其中的每个单元格增加边框。</p>
+      <p>通过添加<code>striped</code>可以给表体之内的每一行增加斑马条纹样式。</p>
+      <p>通过添加<code>condensed</code>可以让表格更加紧凑，单元格中的内补（padding）均会减半。</p>
+      <p>通过添加<code>hover</code>可以让表体中的每一行对鼠标悬停状态作出响应</p>
       <ReactPlayground codeText={Samples.GridStyle} />
     </div>
   );
