d741381470c1bc964048d33708bcfaa063e42a6c
Devin Chen <d3vin.chen@gmail.com>
2017-03-23 12:53:34 +0800

    add className property to DatePicker2 component

--- src/DatePicker2.js
+++ src/DatePicker2.js
@@ -13,24 +13,34 @@ export default class DatePicker2 extends Component {
 
   static propTypes = {
     /**
+     * 日历悬浮窗自定义类名
+     * https://hacker0x01.github.io/react-datepicker/#example-3
+     */
+    calendarClassName: PropTypes.string,
+    /**
+     * 文本框自定义类名
+     * https://hacker0x01.github.io/react-datepicker/#example-2
+     */
+    className: PropTypes.string,
+    /**
      * 日期格式<br>
      * 遵循moment.js格式<br>
      * <a href="https://momentjs.com/docs/#/displaying/format/">Moment.js文档</a>
      */
     dateFormat: PropTypes.string,
     /**
-     * value 请使用ISO 8061格式
-     */
-    value: PropTypes.string,
-    showMonthDropdown: PropTypes.bool,
-    showYearDropdown: PropTypes.bool,
-    /**
      * 参数：
      * - `value {String}`: ISO 8061格式时间字符串
      * - `formattedValue {String}`: 按照用户指定格式进行了格式化后的字符串
      * - `momentDate {Object}`: moment.js对象
      */
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    showMonthDropdown: PropTypes.bool,
+    showYearDropdown: PropTypes.bool,
+    /**
+     * value 请使用ISO 8061格式
+     */
+    value: PropTypes.string
   };
 
   state = {
@@ -55,10 +65,15 @@ export default class DatePicker2 extends Component {
     // 之前使用otherProps获取react-datepicker的属性，然后往下传
     // 但是出现了bug#11
     const { value, showMonthDropdown, showYearDropdown } = this.props;
-    return (<ReactDatePicker
-      showMonthDropdown={showMonthDropdown}
-      showYearDropdown={showYearDropdown}
-      selected={value ? moment(value) : null}
-      onChange={this.handleChange.bind(this)} />);
+    return (
+      <ReactDatePicker
+        className={this.props.className}
+        calendarClassName={this.props.calendarClassName}
+        showMonthDropdown={showMonthDropdown}
+        showYearDropdown={showYearDropdown}
+        selected={value ? moment(value) : null}
+        onChange={this.handleChange.bind(this)}
+      />
+    );
   }
 }
