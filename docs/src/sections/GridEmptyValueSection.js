ec67036a17f874742d4009729fdfd74dd6581548
Devin Chen <d3vin.chen@gmail.com>
2017-04-24 14:34:33 +0800

    refine document

--- docs/src/sections/GridEmptyValueSection.js
+++ docs/src/sections/GridEmptyValueSection.js
@@ -8,8 +8,10 @@ export default function GridEmptyValueSection() {
   return (
     <div className="bs-docs-section">
       <h3><Anchor id="grid-empty-value">演示空值</Anchor></h3>
-      <p>演示传入的<code>null</code>或者<code>undefined</code>，
-      或者有一行没有任何数据，比如<code>{}</code></p>
+      <p>演示传入空值</p>
+      <p>第一行所有列都没有数据，比如<code>{`{}`}</code></p>
+      <p>第二行所有列都是<code>undefined</code>，比如<code>{`{"name": undefined, "code": undefined}`}</code></p>
+      <p>第三行所有列都是<code>null</code>，比如<code>{`{"name": undefined, "code": undefined}`}</code></p>
       <ReactPlayground codeText={Samples.GridEmptyValue} />
     </div>
   );
