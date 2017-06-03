2ed5a2cda675d51e299ec9784bf3bc9f11918732
Devin Chen <d3vin.chen@gmail.com>
2017-06-03 17:30:22 +0800

    ref #70 add react-intl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,11 @@
 > 2017-06-03
 
 - **Feature:** Support click and double click event on table cell and row. ([#69])
+- **Feature:** Add react-intl package to internationalize React apps. ([#70])
 
 [master]: https://github.com/yyssc/ssc-grid/compare/v0.69.3...master
 [#69]: https://github.com/yyssc/ssc-grid/issues/69
+[#70]: https://github.com/yyssc/ssc-grid/issues/70
 
 ## [v0.69.3]
 > 2017-06-02
--- docs/src/sections/GridClickEventSection.js
+++ docs/src/sections/GridClickEventSection.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { IntlProvider } from 'react-intl';
 
 import Anchor from '../Anchor';
 import ReactPlayground from '../ReactPlayground';
 import Samples from '../Samples';
 
+const messages = {
+  'doc.grid-click-event.title': '鼠标点击事件',
+  'doc.grid-click-event.body': '先调用onCellClick然后再调用onRowClick',
+};
+
 export default function GridClickEventSection() {
   return (
-    <div className="bs-docs-section">
-      <h3><Anchor id="grid-click-event">Mouse click events</Anchor></h3>
-      <p>
-        The event handlers below are all triggered by events in the bubbling phase.
-        This means <code>onCellClick</code> will be called before <code>onRowClick</code>,
-        because <code>onCellClick</code> is registered on <code>td</code> tag
-        and <code>onRowClick</code> is registered on <code>tr</code> tag.
-      </p>
-      <ReactPlayground codeText={Samples.GridClickEvent} />
-    </div>
+    <IntlProvider
+      locale="en"
+      messages={messages}
+    >
+      <div className="bs-docs-section">
+        <h3>
+          <Anchor id="grid-click-event">
+            <FormattedMessage
+              id="doc.grid-click-event.title"
+              description="Mouse click event documents"
+              defaultMessage={`Mouse click events`}
+            />
+          </Anchor>
+        </h3>
+        <p>
+          <FormattedMessage
+              id="doc.grid-click-event.body"
+              description="Mouse click event documents"
+              defaultMessage={`The event handlers below are all triggered by events in the bubbling phase.
+  This means {onCellClick} will be called before {onRowClick},
+  because {onCellClick} is registered on {td} tag
+  and {onRowClick} is registered on {tr} tag.`}
+              values={{
+                onCellClick: <code>onCellClick</code>,
+                onCellDoubleClick: <code>onCellClick</code>,
+                onRowClick: <code>onCellClick</code>,
+                onRowDoubleClick: <code>onCellClick</code>,
+                td: <code>td</code>,
+                tr: <code>tr</code>,
+              }}
+          />
+        </p>
+        <ReactPlayground codeText={Samples.GridClickEvent} />
+      </div>
+    </IntlProvider>
   );
 }
