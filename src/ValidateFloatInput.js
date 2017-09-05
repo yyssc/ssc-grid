401fe97b549374c1aaa0f460b38cfad21b91f8c4
Devin Chen <d3vin.chen@gmail.com>
2017-09-06 02:36:23 +0800

    ValidateFloatInput

--- src/ValidateFloatInput.js
+++ src/ValidateFloatInput.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-var, curly, vars-on-top, func-names, one-var, no-unused-vars, guard-for-in */
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
@@ -17,40 +19,284 @@ import * as validationUtils from './utils/validation';
 
 import TextField from './TextField';
 
-// Use a stripped-down indexOf as it's faster than native
-// http://jsperf.com/thor-indexof-vs-for/5
-function indexOf( list, elem ) {
-  let i = 0;
-  let len = list.length;
-  for ( ; i < len; i++ ) {
-    if ( list[i] === elem ) {
-      return i;
-    }
-  }
-  return -1;
+// // Support: Android<4.1, IE<9
+// // Make sure we trim BOM and NBSP
+// const rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+
+// // Support: Android<4.1, IE<9
+// function $trim( text ) {
+//   return text == null ?
+//     '' :
+//     ( text + '' ).replace( rtrim, '' );
+// }
+
+function isEmpty(val) {
+  // return val === '' || val === undefined || val === null || val === $trim(this.$element.attr('tip'));
+  return val === '' || val === undefined || val === null;
 }
 
-function $inArray( elem, arr, i ) {
-  let len;
+var enumerables = true;
+var enumerablesTest = {toString: 1};
 
-  if ( arr ) {
-    if ( indexOf ) {
-      return indexOf.call( arr, elem, i );
-    }
+for (var I in enumerablesTest) {
+  enumerables = null;
+}
+if (enumerables) {
+  enumerables = ['hasOwnProperty', 'valueOf', 'isPrototypeOf', 'propertyIsEnumerable',
+    'toLocaleString', 'toString', 'constructor'];
+}
 
-    len = arr.length;
-    i = i ? i < 0 ? Math.max( 0, len + i ) : i : 0; // eslint-disable-line no-nested-ternary
+// u.biz.js
+const u = {
+  isNumber: (obj) => {
+    // return obj === +obj
+    return (obj - parseFloat( obj ) + 1) >= 0;
+  }
+};
 
-    for ( ; i < len; i++ ) {
-      // Skip accessing in sparse arrays
-      if ( i in arr && arr[ i ] === elem ) {
-        return i;
+/**
+ * 复制对象属性
+ *
+ * @param {Object}  目标对象
+ * @param {config} 源对象
+ */
+u.extend = function(object, config) {
+  var args = arguments, options;
+  if (args.length > 1) {
+    for (var len = 1; len < args.length; len++) {
+      options = args[len];
+      if (object && options && typeof options === 'object') {
+        var i, j, k;
+        for (i in options) {
+          object[i] = options[i];
+        }
+        if (enumerables) {
+          for (j = enumerables.length; j--;) {
+            k = enumerables[j];
+            if (options.hasOwnProperty && options.hasOwnProperty(k)) {
+              object[k] = options[k];
+            }
+          }
+        }
       }
     }
   }
+  return object;
+};
 
-  return -1;
+/**
+ * 抽象格式化类
+ */
+function AbstractMasker() {}
+
+AbstractMasker.prototype.format = function format(obj) {
+  if (obj == null) {
+    return null;
+  }
+
+  const fObj = this.formatArgument(obj);
+  return this.innerFormat(fObj);
+};
+
+/**
+ * 统一被格式化对象结构
+ *
+ * @param obj
+ * @return
+ */
+AbstractMasker.prototype.formatArgument = function formatArgument(/* obj */) {
+
+};
+
+/**
+ * 格式化
+ *
+ * @param obj
+ * @return
+ */
+AbstractMasker.prototype.innerFormat = function innerFormat(/* obj */) {
+
+};
+
+/**
+ *格式结果
+ */
+function FormatResult(value, color) {
+  this.value = value;
+  this.color = color;
 }
+// FormatResult.prototype = new Object;
+FormatResult.prototype = {};
+
+/**
+ * <b> 数字格式化  </b>
+ *
+ * <p> 格式化数字
+ *
+ * </p>
+ *
+ * Create at 2009-3-20 上午08:50:32
+ *
+ * @author bq
+ * @since V6.0
+ */
+
+/**
+ * 构造方法
+ */
+function NumberMasker(formatMeta) {
+  this.update(formatMeta);
+}
+
+NumberMasker.DefaultFormatMeta = {
+  isNegRed: true,
+  isMarkEnable: true,
+  markSymbol: ',',
+  pointSymbol: '.',
+  positiveFormat: 'n',
+  negativeFormat: '-n'
+};
+
+NumberMasker.prototype = new AbstractMasker;
+NumberMasker.prototype.formatMeta = null;
+
+NumberMasker.prototype.update = function(formatMeta) {
+  this.formatMeta = u.extend({}, NumberMasker.DefaultFormatMeta, formatMeta);
+};
+
+/**
+ *格式化对象
+ */
+NumberMasker.prototype.innerFormat = function(obj) {
+  var dValue, express, seperatorIndex, strValue;
+  dValue = obj.value;
+  if (dValue > 0) {
+    express = this.formatMeta.positiveFormat;
+    strValue = dValue + '';
+  } else if (dValue < 0) {
+    express = this.formatMeta.negativeFormat;
+    strValue = (dValue + '').substr(1, (dValue + '').length - 1);
+  } else {
+    express = this.formatMeta.positiveFormat;
+    strValue = dValue + '';
+  }
+  seperatorIndex = strValue.indexOf('.');
+  strValue = this.setTheSeperator(strValue, seperatorIndex);
+  strValue = this.setTheMark(strValue, seperatorIndex);
+  var color = null;
+  if (dValue < 0 && this.formatMeta.isNegRed) {
+    color = 'FF0000';
+  }
+
+  /**
+   * 将AFindText全部替换为ARepText
+   */
+  String.prototype.replaceAll = function(AFindText, ARepText) { // eslint-disable-line no-extend-native
+    // 自定义String对象的方法
+    var raRegExp = new RegExp(AFindText, 'g');
+    return this.replace(raRegExp, ARepText);
+  };
+
+  return new FormatResult(express.replaceAll('n', strValue), color);
+};
+
+/**
+ *设置标记
+*/
+NumberMasker.prototype.setTheMark = function(str, seperatorIndex) {
+  var endIndex, first, index;
+  if (!this.formatMeta.isMarkEnable)
+    return str;
+  if (seperatorIndex <= 0)
+    seperatorIndex = str.length;
+  first = str.charCodeAt(0);
+  endIndex = 0;
+  if (first === 45)
+    endIndex = 1;
+  index = seperatorIndex - 3;
+  while (index > endIndex) {
+    str = str.substr(0, index - 0) + this.formatMeta.markSymbol + str.substr(index, str.length - index);
+    index = index - 3;
+  }
+  return str;
+};
+NumberMasker.prototype.setTheSeperator = function(str, seperatorIndex) {
+  var ca;
+  if (seperatorIndex > 0) {
+    ca = NumberMasker.toCharArray(str);
+    // ca[seperatorIndex] = NumberMasker.toCharArray(this.formatMeta.pointSymbol)[0];
+    ca[seperatorIndex] = this.formatMeta.pointSymbol;
+    str = ca.join('');
+  }
+  return str;
+};
+/**
+ * 将字符串转换成char数组
+ * @param {} str
+ * @return {}
+ */
+NumberMasker.toCharArray = function(str2) {
+  var str = str2.split('');
+  var charArray = new Array();
+  for (var i = 0; i < str.length; i++) {
+    charArray.push(str[i]);
+  }
+  return charArray;
+};
+
+/**
+ *默认构造方法
+ */
+NumberMasker.prototype.formatArgument = function(obj) {
+  var numberObj = {};
+  numberObj.value = obj;
+  return numberObj;
+};
+
+u.NumberMasker = NumberMasker;
+
+/**
+ * 数据格式化工具
+ */
+
+function NumberFormater(precision) {
+  this.precision = precision;
+}
+
+NumberFormater.prototype.update = function update(precision) {
+  this.precision = precision;
+};
+
+NumberFormater.prototype.format = function format(value) {
+  if (!u.isNumber(value)) return '';
+
+    // 以0开头的数字将其前面的0去掉
+  while ((value + '').charAt(0) === '0' && value.length > 1 && (value + '').indexOf('0.') !== 0) {
+    value = value.substring(1);
+  }
+  let result = value;
+  if (u.isNumber(this.precision)) {
+    // TODO 需要引入BigNumber.js
+    // bignumber.js v2.0.7 https://github.com/MikeMcl/bignumber.js
+    if (window.BigNumber && !( (value.toString().indexOf('.') > -1) && (value.toString().replace( '.', '' ).length > 15) ) ) {
+      // 已经引入BigNumber,并且满足bigNum最大只支持15位有效数字
+      result = (new window.BigNumber(value)).toFixed(this.precision);
+    } else {
+      const digit = parseFloat(value);
+      // 解决toFixed四舍五入问题，如1.345
+      result = (Math.round(digit * Math.pow(10, this.precision)) / Math.pow(10, this.precision)).toFixed(this.precision);
+    }
+    if (result === 'NaN') {
+      return '';
+    }
+  }
+
+  return result;
+};
+
+u.NumberFormater = NumberFormater;
+
+const regExp = /^-?\d+(\.\d+)?$/;
 
 const propTypes = {
   /**
@@ -62,6 +308,8 @@ const propTypes = {
    * 是否禁用输入框
    */
   disabled: PropTypes.bool,
+  max: PropTypes.number,
+  min: PropTypes.number,
   /**
    * 当光标离开输入框
    */
@@ -79,53 +327,9 @@ const propTypes = {
    */
   placeholder: PropTypes.string,
   /**
-   * 带有校验功能的输入框
-   * 场景1：使用内置校验（定义在`src/utils/validation.js`）
-   * 比如`email`为校验邮件地址格式
-   * ```js
-   * {
-   *   type: 'email'
-   * }
-   * ```
-   * `type`可以是：
-   * - `currency` 货币金额格式
-   * - `decimal` 十进制数字格式
-   * - `email` 电子邮件格式
-   * - `int` 整数类型
-   * - `length` 输入文本的长度
-   * - `mobilePhone` 手机号码格式，比如18911112222
-   * - `required` 必输字段
-   * 场景2：使用自定义校验
-   * 比如校验用户输入数字的范围
-   * ```js
-   * {
-   *   type: 'custom',
-   *   matchFunc: (value, validator) => parseInt(value, 10) <= 100 && parseInt(value, 10) >= 0,
-   *   helpText: (value, validator) => '残值率不能大于100%，小于0%'
-   * }
-   * ```
-   * 回调函数`matchFunc(string, Object) => boolean`用于对文本框中的文字进行校验，返回false则校验失败，显示`helpText`定义的错误信息
-   * 回调函数`helpText(string, Object) => string`用于显示校验错误时候的错误提示信息
+   * Required field
    */
-  validators: PropTypes.arrayOf(
-    PropTypes.oneOfType([
-      PropTypes.shape({
-        type: PropTypes.oneOf(['currency', 'decimal', 'email', 'int', 'mobilePhone', 'required']).isRequired,
-      }),
-      PropTypes.shape({
-        type: PropTypes.oneOf(['length']).isRequired,
-        options: PropTypes.shape({
-          min: PropTypes.number.isRequired,
-          max: PropTypes.number.isRequired,
-        }),
-      }),
-      PropTypes.shape({
-        type: PropTypes.oneOf(['custom']),
-        matchFunc: PropTypes.func.isRequired,
-        helpText: PropTypes.func.isRequired,
-      })
-    ])
-  ).isRequired,
+  required: PropTypes.bool,
   /**
    * 文本框中显示的值
    * 注意：由于文本框是完全自由输入的，所以value的类型，以及在matchFunc回调函数的value参数的类型
@@ -136,7 +340,9 @@ const propTypes = {
 };
 
 const defaultProps = {
-  validators: [],
+  max: null,
+  min: null,
+  required: false,
   value: '',
 };
 
@@ -149,22 +355,18 @@ export default class ValidateFloatInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      /**
-       * 帮助信息文本
-       * @type {String}
-       */
-      helpText: '',
-      /**
-       * Set validationState to one of 'success', 'warning' or 'error' to show
-       * validation state. Set validationState to null (or undefined) to hide
-       * validation state
-       * @type {String}
-       */
-      validationState: null,
+      showValue: '',
+      trueValue: '',
     };
     this.handleBlur = this.handleBlur.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleFocus = this.handleFocus.bind(this);
+
+    this.maskerMeta = {
+      precision: 2
+    };
+    this.formater = new u.NumberFormater(this.maskerMeta.precision);
+    this.masker = new u.NumberMasker(this.maskerMeta);
   }
 
   /**
@@ -173,53 +375,99 @@ export default class ValidateFloatInput extends Component {
   componentWillReceiveProps() {
   }
 
-  // 提供父组件可以清空校验状态
-  reset() {
+  setValue(val) {
+    const value = parseFloat(val);
+    // this.maskerMeta.precision = this.dataModel.getRowMeta(this.field, 'precision') || this.maskerMeta.precision;
+    // this.formater.precision = this.maskerMeta.precision;
+    // if (this.dataModel.getCurrentRow() == null) {
+    //   return;
+    // }
+    // const mvalue = this.dataModel.getCurrentRow().getValue(this.field);
+    const mvalue = this.state.trueValue;
+    if (value === mvalue) {
+      this.setState({
+        showValue: this.masker.format(this.state.trueValue).value
+      });
+    } else {
+      const trueValue = this.formater.format(value);
+      this.setState({
+        trueValue,
+        showValue: this.masker.format(trueValue).value,
+      });
+    }
+  }
+
+  doValid(pValue) {
     this.setState({
-      helpText: '',
-      validationState: null,
+      needClean: false
     });
+    let value = pValue;
+
+    if (isEmpty(value) && this.props.required) {
+      // this.showMsg(this.nullMsg); // TODO
+      return false;
+    } else if (isEmpty(value) && !this.props.required) {
+      return true;
+    }
+    if (regExp) {
+      const reg = new RegExp(regExp);
+      if (typeof value === 'number') {
+        value = value + '';
+      }
+      const r = value.match(reg);
+      if (r === null || r === false) {
+        // this.showMsg(this.errorMsg); // TODO
+        this.setState({
+          needClean: true
+        });
+        return false;
+      }
+    }
+    if (this.props.max !== undefined && this.props.max != null) {
+      if (parseFloat(value) > this.props.max) {
+        this.setState({
+          errorMsg: '输入值不能大于' + this.max
+        });
+        // this.showMsg(this.errorMsg); // TODO
+        return false;
+      }
+    }
+    if (this.props.min !== undefined && this.props.min != null) {
+      if (parseFloat(value) < this.props.min) {
+        this.setState({
+          errorMsg: '输入值不能小于' + this.props.min
+        });
+        // this.showMsg(this.errorMsg); // TODO
+        return false;
+      }
+    }
+    return true;
   }
 
   /**
-   * 父级组件主动校验
-   * @return {booean} 校验成功还是失败
+   * 是否需要清除数据
+   *
+   * @returns {boolean}
+   * @memberof ValidateFloatInput
    */
-  doValidate() {
-    return this.setValidationState(this.textFieldRef.state.value);
-  }
-
-  /**
-   * 校验状态并设置组件状态
-   * @param {String} value 文本框内的值
-   * @return {boolean} 校验成功还是失败
-   */
-  setValidationState(value) {
-    const {
-      validationState,
-      helpText
-    } = validationUtils.calcValidationState(value, this.props.validators);
-    this.setState({ helpText, validationState });
-    return validationState === 'success';
+  _needClean() {
+    return true;
   }
 
   handleBlur(event) {
-    if (!self.doValidate() && self._needClean()) {
-      if (!self.doValidate() && self._needClean()) {
-        if (self.required && (self.element.value === null || self.element.value === undefined || self.element.value === '')) {
-            // 因必输项清空导致检验没通过的情况
-            // self.setValue('');
-          this.setState({
-            value: '',
-          });
-        } else {
-          self.element.value = self.getShowValue();
-        }
+    if (!this.doValid() && this._needClean()) {
+      if (this.props.required && (event.target.value === null || event.target.value === undefined || event.target.value === '')) {
+        // 因必输项清空导致检验没通过的情况
+        this.setValue('');
+      } else {
+        this.setState({
+          showValue: this.state.showValue
+        });
       }
     } else {
-      self.setValue(self.element.value);
+      console.log('没有校验通过');
+      this.setValue(event.target.value);
     }
-
 
     if (this.props.onBlur) {
       this.props.onBlur(event);
@@ -229,8 +477,9 @@ export default class ValidateFloatInput extends Component {
   handleChange(event) {
     const { value } = event.target;
 
-    // this.setState({ value });
-    this.setValidationState(value);
+    this.setState({
+      showValue: value,
+    });
 
     if (this.props.onChange) {
       this.props.onChange(value);
@@ -238,11 +487,36 @@ export default class ValidateFloatInput extends Component {
   }
 
   handleFocus(event) {
+    var v = this.state.trueValue, vstr = v + '', focusValue = v;
+    if (u.isNumber(v) && u.isNumber(this.maskerMeta.precision)) {
+      if (vstr.indexOf('.') >= 0) {
+        var sub = vstr.substr(vstr.indexOf('.') + 1);
+        if (sub.length < this.maskerMeta.precision || parseInt(sub.substr(this.maskerMeta.precision), 10) === 0 || this.maskerMeta.precision === 0) {
+          focusValue = this.formater.format(v);
+        }
+      } else if (this.maskerMeta.precision > 0) {
+        focusValue = this.formater.format(v);
+      }
+    }
+    focusValue = parseFloat(focusValue) || '';
+    this.setState({
+      showValue: focusValue
+    });
+
     if (this.props.onFocus) {
       this.props.onFocus(event);
     }
   }
 
+  /**
+   * event.keyCode:
+   * - 48~57 数字0~9
+   * - 190 小数点
+   *
+   * @param {any} event
+   * @returns
+   * @memberof ValidateFloatInput
+   */
   handleKeyDown(event) {
     // copy from u.js
     let tmp = event.target.value;
@@ -256,7 +530,7 @@ export default class ValidateFloatInput extends Component {
     } else if (event.ctrlKey && (event.keyCode === 67 || event.keyCode === 86)) {
       // 复制粘贴
       return true;
-    } else if (!((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105) || ($inArray(event.keyCode, [8, 110, 190, 189, 109]) > -1))) {
+    } else if (!((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105) || ([8, 110, 190, 189, 109].indexOf(event.keyCode) > -1))) {
       // event.returnValue = false;
       event.preventDefault();
       return false;
@@ -279,7 +553,7 @@ export default class ValidateFloatInput extends Component {
       >
         <TextField
           ref={(c) => this.textFieldRef = c}
-          value={this.props.value}
+          value={this.state.showValue}
           disabled={this.props.disabled}
           onBlur={this.handleBlur}
           onChange={this.handleChange}
