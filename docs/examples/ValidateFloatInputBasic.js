7ab600f171db4530a4fc2e6e9267d93deb07edb1
Devin Chen <d3vin.chen@gmail.com>
2017-09-06 16:16:41 +0800

    add example for ValidateFloatInput

--- docs/examples/ValidateFloatInputBasic.js
+++ docs/examples/ValidateFloatInputBasic.js
@@ -2,7 +2,14 @@ const ValidateFloatInputBasicExample = React.createClass({
   render() {
     return (
       <div>
+        缺省值
         <ValidateFloatInput />
+        给定初始值
+        <ValidateFloatInput value="123" />
+        必选项
+        <ValidateFloatInput required />
+        数值长度校验
+        <ValidateFloatInput min={1} max={9} />
       </div>
     );
   }
